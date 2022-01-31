953e9cfa67141dfc115381977dfe7755ab5ad034
DamirL <dmr.lutfullin@gmail.com>
2022-01-31 08:12:41 +0500

    Initial Commit

---
+++ BowTies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BowTies
+//
+//  Created by Damir L on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
